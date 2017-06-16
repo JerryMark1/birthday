98fc3389c8247e280207de4b14615255ce0bd477
jerry <676557432@qq.com>
2017-06-16 11:10:55 +0800

    update

--- js/diy.js
+++ js/diy.js
@@ -5,4 +5,21 @@ $(function(){
         'navigationColor':['#fff'],
 
     });
+     var audio = $('#myaudio');
+    var isPlaying = false;
+    function playAudio() {
+        var audio = $('#myaudio');
+        if (audio.attr('src') == undefined) {
+            audio.attr('src', audio.data('src'));
+        }
+        audio[0].play();
+        isPlaying = true;
+    }
+    playAudio();
+    document.addEventListener("WeixinJSBridgeReady", function () {
+        WeixinJSBridge.invoke('getNetworkType', {}, function (e) {
+            network = e.err_msg.split(":")[1];  //结果在这里
+        playAudio();
+        });
+    }, false);
 });
